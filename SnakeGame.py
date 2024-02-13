b8cb1acbbfbeae68c34f3b2c9634a54cc4ffe0d7
Enica King <127141001+enicaking@users.noreply.github.com>
2024-02-13 10:31:57 +0100

    Time to use available directions

--- SnakeGame.py
+++ SnakeGame.py
@@ -13,7 +13,7 @@ import pygame, sys, time, random, csv
 # Hard      ->  40
 # Harder    ->  60
 # Impossible->  120
-DIFFICULTY = 10
+DIFFICULTY = 30
 
 # Window size
 FRAME_SIZE_X = 480
@@ -35,15 +35,24 @@ class GameState:
         ################# food in limit of frame size -> cannot go there (game_over)
         self.food_pos = [random.randrange(1, (FRAME_SIZE[0] // 10)) * 10,
                          random.randrange(1, (FRAME_SIZE[1] // 10)) * 10]
+        self.food_positions = [[290, 450], [120, 110], [20, 10], [210, 110], [360, 140], [440, 410], [130, 60], [340, 380],
+                    [230, 260], [180, 20], [350, 150], [380, 250], [260, 310], [280, 40]]
+        self.food_pos = [220, 390]
+        print('-------------------------------------------------------------------------------new food', self.food_pos  )
         self.food_spawn = True
         self.direction = 'RIGHT'
+        self.prev_direction = None
         self.change_to = self.direction
         self.score = 0
+        self.food_score = 0
 
-        self.dist_x = self.food_pos[0] - self.snake_body[0][0]
-        self.dist_y = self.food_pos[1] - self.snake_body[0][1]
+        self.dist_x = (self.food_pos[0] - self.snake_body[0][0])/10
+        self.dist_y = (self.food_pos[1] - self.snake_body[0][1])/10
 
-        self.predictedpath = []
+        self.available_left = True
+        self.available_right = True
+        self.available_up = True
+        self.available_down = True
 
 # Game Over
 def game_over(game):
@@ -90,146 +99,134 @@ def move_keyboard(game, event):
     return change_to
 
 
-# THE NEW INTELLIGENT METHOD
+# TODO: IMPLEMENT HERE THE NEW INTELLIGENT METHOD
 
-def isBlockedWall(game):
-    body_blocked = False
-    # Bump itself
-    #for block in game.snake_body[1:]:
-        #if game.snake_pos[0] == block[0] and game.snake_pos[1] == block[1]:
-            # body_blocked = True
-    # Bump wall
-    if (game.snake_pos[0] == 0 or game.snake_pos[0] == FRAME_SIZE_X
-        or game.snake_pos[1] == 0 or game.snake_pos[1] == FRAME_SIZE_Y):
-        body_blocked = True
-    return body_blocked
-
-def isBlockedBody(game):
-    body_blocked = False
-    # Bump itself
-    #for block in game.snake_body[1:]:
-        #if game.snake_pos[0] == block[0] and game.snake_pos[1] == block[1]:
-            # body_blocked = True
-    return body_blocked
-
-
-def predictPath(game):
-    predictedpath = []
-    snake_head = game.snake_body[0]
-    while not (snake_head[0] == game.food_pos[0] and snake_head[1] == game.food_pos[1]):
-        new_direction = choose_snake_direction(game, snake_head)
-        next_step = move_snake_head(new_direction, snake_head)
-        predictedpath.append(next_step)
-    return predictedpath
-
-
-def choose_snake_direction(game, snake_head):
-    change_to = game.direction
-    if not isBlockedWall(game):
-        if game.food_pos[0] == snake_head[0]:
-            # food and snake are at the same x coordinate
-            if game.food_pos[1] < snake_head[1] and game.direction != 'DOWN':
-                change_to = 'UP'
-            elif game.food_pos[1] > snake_head[1] and game.direction != 'UP':
-                change_to = 'DOWN'
-            else:
-                change_to = loop(game, False)
-
+def loop(game, axis, change_to):
+    # axis == True -> it has to do a loop in the x axis
+    # axis == False -> it has to do a loop in the y axis
+    if axis:
+        # by default it will go down, if that means getting out of bounds it will go up
+        if game.snake_pos[1] == FRAME_SIZE_Y:
+            change_to = 'UP'
         else:
-            if game.food_pos[0] < snake_head[0] and game.direction != 'RIGHT':
-                change_to = 'LEFT'
-            elif game.food_pos[0] > snake_head[0] and game.direction != 'LEFT':
-                change_to = 'RIGHT'
-            else:
-                change_to = loop(game, True)
+            change_to = 'DOWN'
+    else:
+        # by default it will go right, if that means getting out of bounds it will go left
+        if game.snake_pos[0] == FRAME_SIZE_X:
+            change_to = 'LEFT'
+        else:
+            change_to = 'RIGHT'
 
     return change_to
 
 
-def move_snake_head(direction, snake_head):
-    if direction == 'UP':
-        snake_head[1] -= 10
-    elif direction == 'DOWN':
-        snake_head[1] += 10
-    elif direction == 'LEFT':
-        snake_head[0] -= 10
-    elif direction == 'RIGHT':
-        snake_head[0] += 10
-    return snake_head
+def new_position(game, change_to):
+    new_body = game.snake_body.copy()
+    new_pos = game.snake_pos.copy()
 
+    if game.direction == 'UP':
+        new_pos[1] -= 10
+    elif game.direction == 'DOWN':
+        new_pos[1] += 10
+    elif game.direction == 'LEFT':
+        new_pos[0] -= 10
+    elif game.direction == 'RIGHT':
+        new_pos[0] += 10
 
-def loop(game, axis):
-    # axis == True -> it has to do a loop in the x axis
-    # axis == False -> it has to do a loop in the y axis
-    change_to = game.direction
-    if axis:
-        # by default it will go down, if that means getting out of bounds it will go up
-        change_to = 'DOWN'
-        if game.snake_pos[1] == FRAME_SIZE_Y:
-            change_to = 'UP'
-    else:
-        # by default it will go right, if that means getting out of bounds it will go left
-        change_to = 'RIGHT'
-        if game.snake_pos[0] == FRAME_SIZE_X:
-            change_to = 'LEFT'
+    new_body.insert(0, list(new_pos))
+    new_body.pop()
+
+    body_blocked_wall = False
+    body_blocked = False
+
+    # Bump itself
+    for block in new_body[1:]:
+        if new_pos[0] == block[0] and new_pos[1] == block[1]:
+            body_blocked = True
+            print('checking bump itself')
+
+    # Bump wall
+    if (new_pos[0] < 0 or new_pos[0] > FRAME_SIZE_X
+            or new_pos[1] < 0 or new_pos[1] > FRAME_SIZE_Y):
+        body_blocked_wall = True
+        print('checking bump wall')
+
+    if body_blocked_wall:
+        print('body blocked -> loop')
+        if change_to == 'RIGHT' or change_to == 'LEFT':
+            print('x loop')
+            change_to = loop(game, True, change_to)
+        else:
+            print('y loop')
+            change_to = loop(game, False, change_to)
+    elif body_blocked:
+        print('body blocked -> bordear')
+
+        change_to = game.prev_direction
+
     return change_to
 
 
 def move_tutorial_1(game):
     change_to = game.direction
-    snake_moves_head = game.snake_pos
-    if not isBlockedWall(game):
-        if game.food_pos[0] == game.snake_body[0][0]:
+    game.prev_direction = game.direction
+
+    # if it won't bump any wall or collapse with itself, change direction
+    if game.food_pos[0] == game.snake_body[0][0]:
             # food and snake are at the same x coordinate
             if game.food_pos[1] < game.snake_body[0][1] and game.direction != 'DOWN':
                 change_to = 'UP'
-                #snake_moves_head[1] -= 10
+
             elif game.food_pos[1] > game.snake_body[0][1] and game.direction != 'UP':
                 change_to = 'DOWN'
-                #snake_moves_head[1] += 10
-            else:
-                change_to = loop(game, False)
-            #for block in game.snake_body[1:(len(game.snake_body)-1)]:
-                #if snake_moves_head[1] == block[1]:
-                    #change_to = loop(game, False)
 
-        else:
+            else:
+                change_to = loop(game, False, change_to)
+                print('loop y')
+
+    else:
             if game.food_pos[0] < game.snake_body[0][0] and game.direction != 'RIGHT':
                 change_to = 'LEFT'
-                #snake_moves_head[0] -= 10
+
             elif game.food_pos[0] > game.snake_body[0][0] and game.direction != 'LEFT':
                 change_to = 'RIGHT'
-                #snake_moves_head[0] += 10
-            else:
-                change_to = loop(game, True)
-            #for block in game.snake_body[1:(len(game.snake_body)-1)]:
-                #if snake_moves_head[0] == block[0]:
-                    #change_to = loop(game, True)
 
-    return change_to
+            else:
+                change_to = loop(game, True, change_to)
+                print('loop x')
+
+    return new_position(game, change_to)
 
 
 # PRINTING DATA FROM GAME STATE
 def print_state(game):
     print("--------GAME STATE--------")
-    print("FrameSize:", FRAME_SIZE_X, FRAME_SIZE_Y)
+    #print("FrameSize:", FRAME_SIZE_X, FRAME_SIZE_Y)
     print("Direction:", game.direction)
+    print("Previous Direction:", game.prev_direction)
     print("Snake X:", game.snake_pos[0], ", Snake Y:", game.snake_pos[1])
-    print("Snake Body:", game.snake_body)
+    #print("Snake Body:", game.snake_body)
+    print("Snake Body Length:", len(game.snake_body))
     print("Food X:", game.food_pos[0], ", Food Y:", game.food_pos[1])
-    print("Score:", game.score)
-    print("Blocked?", isBlockedWall(game))
+    #print("Score:", game.score)
+    print("Food Score:", game.food_score)
+    print("Left available direction?", game.available_left)
+    print("Right available direction?", game.available_right)
+    print("Up available direction?", game.available_up)
+    print("Down available direction?", game.available_down)
+
 
     print("Distance to food:", game.dist_x, game.dist_y)
-    print("Predicted Path:", game.predictedpath)
+
 
 
 # TODO: IMPLEMENT HERE THE NEW INTELLIGENT METHOD
 def print_line_data(game):
     line_data = [FRAME_SIZE_X, FRAME_SIZE_Y, game.direction,
-                 game.snake_pos[0], game.snake_pos[1], game.snake_body,
-                 game.food_pos[0], game.food_pos[1], game.score,
-                 isBlockedWall(game), game.dist_x, game.dist_y]
+                game.snake_pos[0], game.snake_pos[1], len(game.snake_body),
+                game.food_pos[0], game.food_pos[1], game.score,
+                game.dist_x, game.dist_y, game.food_score,
+                game.available_left, game.available_right, game.available_up, game.available_down]
     with open("C:\\Users\\bucky\\Documents\\UC3M\\YEAR TWO - SPRING\\Machine Learning I\\Tutorial 1\\snakeGame.csv", "a",
               newline='') as file:
         writer = csv.writer(file)
@@ -255,6 +252,8 @@ fps_controller = pygame.time.Clock()
 
 # Main logic
 game = GameState((FRAME_SIZE_X, FRAME_SIZE_Y))
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -267,6 +266,7 @@ while True:
         # CALLING MOVE METHOD
         # game.direction = move_tutorial_1(game)
 
+    # UNCOMMENT WHEN METHOD IS IMPLEMENTED
     game.direction = move_tutorial_1(game)
 
     if game.direction == 'UP':
@@ -288,10 +288,14 @@ while True:
         game.snake_body.pop()
         game.score -= 1
 
+
     # Spawning food on the screen
     if not game.food_spawn:
         game.food_pos = [random.randrange(1, (FRAME_SIZE_X // 10)) * 10, random.randrange(1, (FRAME_SIZE_Y // 10)) * 10]
-        game.predictedpath = predictPath(game)
+        #game.food_pos = game.food_positions.pop(0)
+        print('-------------------------------------------------------------------------------new food', game.food_pos  )
+        game.food_score += 1
+
     game.food_spawn = True
 
     # Calculating distance to the food
@@ -312,13 +316,19 @@ while True:
     # Game Over conditions
     # Getting out of bounds
     if game.snake_pos[0] < 0 or game.snake_pos[0] > FRAME_SIZE_X - 10:
+        print('out of bound')
+
         game_over(game)
     if game.snake_pos[1] < 0 or game.snake_pos[1] > FRAME_SIZE_Y - 10:
+        print('out of bound')
+
         game_over(game)
+
 
     # Touching the snake body
     for block in game.snake_body[1:]:
         if game.snake_pos[0] == block[0] and game.snake_pos[1] == block[1]:
+            print('collapsed')
             game_over(game)
 
     show_score(game, 1, WHITE, 'consolas', 15)
@@ -328,4 +338,6 @@ while True:
     fps_controller.tick(DIFFICULTY)
     # PRINTING STATE AND IN CSV
     print_state(game)
-    print_line_data(game)
+    #print_line_data(game)
+
+
